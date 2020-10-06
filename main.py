abf4b093b1fcc00fb0c03eadbedf2d201f78827f
rpiw <radoslawp95@gmail.com>
2020-10-06 15:46:22 +0200

    Add class for directories, minor changes in Log writing

--- main.py
+++ main.py
@@ -1,14 +1,16 @@
 import datetime
 from enum import Enum, unique
 import pathlib
+import os
 
 from exceptions import UnknownVersionException
-
+from exceptions import DirectoryDoesNotExistsException
 
 class Log:
     u"""Maintain writing a log."""
     instances = []
     _limit = 1000  # Amount of characters to cache
+    _force = False
 
     def __init__(self):
         self.name = "log" + datetime.datetime.now().strftime("%Y_%m_%d_%H%M")
@@ -18,7 +20,9 @@ class Log:
     def write(self):
         with open(self.name, "a") as log:
             for line in self.log:
-                log.write(line + "\n")
+                l = line + "\n"
+                print(l)
+                log.write(l)
         self.log.clear()
 
     def cache(self, data):
@@ -30,6 +34,12 @@ class Log:
         self.log.append(data)
         if len(self.log) >= Log._limit:
             self.write()
+            self._force = False
+        elif self._force:
+            self.write()
+
+    def force_write(self):
+        self._force = True
 
     @staticmethod
     def show_instances():
@@ -39,27 +49,6 @@ class Log:
 class GlobalNameSpace:
     u"""Class for keeping everything not fitting to other classes."""
     known_versions = {0: "enhanced_edition", 1: "diamond_edition"}
-
-
-class Config:
-    u"""Config file."""
-
-    def __init__(self):
-        self.system_type = {0: "linux", 1: "windows", 2: "macOS"}
-        self.system = self.system_type[0]
-        self.diamond_version = None
-        self.enhanced_version = None
-
-
-class NWN:
-    u"""Class recognizing type of the game."""
-    def __init__(self, path_to_dir, version):
-        self.path = NWN.check_path(path_to_dir)
-        try:
-            self.version = NWN.check_version(version)
-        except UnknownVersionException:
-            self.version = ""
-        self.directories = []
 
     @staticmethod
     def check_path(path):
@@ -76,12 +65,82 @@ class NWN:
             log.cache("Directory does not exist or path is invalid.")
             return ""
 
+
+class Config:
+    u"""Config file."""
+    _keys = ("system",
+             "diamond_version", "diamond_version_local_dir",
+             "enhanced_version",  "enhanced_version_local_dir")
+
+    def __init__(self, file="config.json"):
+        self._config = {}
+        self._file = file
+        self.system_type = {0: "linux", 1: "windows", 2: "macOS"}
+        self.system = self.system_type[1]
+        self.diamond_version = ""
+        self.enhanced_version = ""
+        self.diamond_version_local_dir = ""
+        self.enhanced_version_local_dir = ""
+
+    def read_config_file(self):
+        import json
+        try:
+            with open(self._file, "r") as f:
+                self.set_config(json.load(f))
+        except FileNotFoundError:
+            log.cache("Config file not found!")
+
+    def set_config(self, _cfg):
+        self._config = _cfg
+        self.system = _cfg[Config._keys[0]]
+        self.diamond_version = _cfg[Config._keys[1]]
+        self.diamond_version_local_dir = _cfg[Config._keys[2]]
+        self.enhanced_version = _cfg[Config._keys[3]]
+        self.enhanced_version_local_dir = _cfg[Config._keys[4]]
+
+    def get_config(self):
+        return self._config
+
+    def print_properties(self):
+        print(self.system,
+              self.diamond_version, self.diamond_version_local_dir,
+              self.enhanced_version, self.enhanced_version_local_dir)
+
+
+class NWN:
+    u"""Class recognizing type of the game."""
+    def __init__(self, path_to_dir, version):
+        self.path = GlobalNameSpace.check_path(path_to_dir)
+        try:
+            self.version = NWN.check_version(version)
+        except UnknownVersionException:
+            self.version = ""
+        self.directory = Directory(self.path)
+        self.directories = list(d for d in self.directory.listdir if os.DirEntry.is_dir(d))
+        self.files = list(d for d in self.directory.listdir if os.DirEntry.is_file(d))
+
     @staticmethod
     def check_version(version):
         if version not in GlobalNameSpace.known_versions.values():
             raise UnknownVersionException(version)
         else:
             return version
+
+
+class Directory:
+    u"""Represent a directory."""
+
+    def __init__(self, path):
+        p = pathlib.Path(path)
+        if not p.exists():
+            raise DirectoryDoesNotExistsException
+        else:
+            self.path = p
+            self.listdir = []
+
+            with os.scandir(self.path) as it:
+                for entry in it:
+                    self.listdir.append(entry)
 
 
 class File:
@@ -102,11 +161,15 @@ class File:
                    FileType.movie: "bik"
                    }
 
-    def __init__(self, file):
+    def __init__(self, file, directory: Directory):
         self.file = file
+        self.directory = directory
 
 
 if __name__ == '__main__':
     log = Log()
-
-
+    log.force_write()
+    cfg = Config()
+    cfg.read_config_file()
+    c = cfg.get_config()
+    nwn = NWN(c["diamond_version_local_dir"], GlobalNameSpace.known_versions[1])
