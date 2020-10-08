8e929554f4ce57def5beeb1faf0ece821e7f046f
rpiw <radoslawp95@gmail.com>
2020-10-09 01:20:31 +0200

    Extract links from website and return list.

--- scrapper.py
+++ scrapper.py
@@ -1,17 +1,37 @@
 from bs4 import BeautifulSoup
 import requests
-
-website_1 = "http://neverwintervault.org"
-website_2 = "https://neverwintervault.org/article/reference/campaigns-and-module-series-list-nwn1"
+import re
 
 
-class Scrapper:
-    u"""Scrapper for http://neverwintervault.org"""
+class Website:
 
-    def request(self, website):
-        response = requests.get(website)
-        print(response.text)
+    def __init__(self, adress):
+        self.website = adress
+
+    def www(self):
+        return self.website
+
+
+class REPatterns:
+    nwn1 = re.compile("nwn1/module")
+    http = re.compile('^http://')
+
+
+def request(website):
+    response = requests.get(website)
+    soup = BeautifulSoup(response.text, "html.parser")
+    for link in soup.find_all('a', attrs={'href': REPatterns.http}):
+        yield link.get('href')
+
+
+def create_list_of_links():
+    result = []
+    for e in request(website_2.www()):
+        if REPatterns.nwn1.search(e):
+            result.append(e)
+    return result
 
 
 if __name__ == '__main__':
-    Scrapper().request(website_2)
+    website_1 = Website("http://neverwintervault.org")
+    website_2 = Website("https://neverwintervault.org/article/reference/campaigns-and-module-series-list-nwn1")
