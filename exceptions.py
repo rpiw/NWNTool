8e151ca790a2a938e7f67687c34316a06d48a758
rpiw <radoslawp95@gmail.com>
2020-10-12 18:21:27 +0200

    Add parent class with logging.

--- exceptions.py
+++ exceptions.py
@@ -1,24 +1,31 @@
+import logging
+
+
+class GeneralException(Exception):
+
+    def __init__(self, message: str):
+        logging.error(message)
+        super(GeneralException, self).__init__()
+
+
 class UnknownVersionException(Exception):
     """Raised when passed unrecognized argument 'version' to NWN class initializer.
     Attributes:
-        version -- wrong argument passed to initializer.
-    """
+        version -- wrong argument passed to initializer."""
     def __init__(self, version):
-        self.version = version
-        self.message = "Unrecognized version of Neverwinter Nights: {0}".format(self.version)
+        self.message = "Unrecognized version of Neverwinter Nights: {0}".format(version)
         super().__init__(self.message)
 
 
 class DirectoryDoesNotExistsException(Exception):
-    """Raised when passed unexisting path to a Directory class' initializer """
-
+    """Raised when passed not existing path to a Directory class' initializer."""
     def __init__(self, path):
         self.message = "Directory does not exists: {0}".format(path)
         super().__init__(self.message)
 
 
 class InvalidUrl(Exception):
-
+    """Invalid url was provided."""
     def __init__(self):
         self.message = "Address of website is not valid!"
         super(InvalidUrl, self).__init__(self.message)
