12b59dbc10c204f9ee2609532b84df0e02f4f679
rpiw <radoslawp95@gmail.com>
2020-12-24 22:08:12 +0100

    Initialize GUI

--- GUImain.py
+++ GUImain.py
@@ -1,21 +1,33 @@
-from PySide2 import QtCore
-from PySide2.QtWidgets import QApplication, QPushButton, QVBoxLayout, QMainWindow
-from PySide2.QtUiTools import QUiLoader
+from PySide2.QtWidgets import QApplication, QMainWindow, QFileDialog
+from PySide2 import QtGui
+from mainwindow import Ui_MainWindow
 import logging
 import session
+
 
 logger = logging.getLogger(__name__)
 gui_session = session.Session()
 
 
-class MyWidget(QMainWindow):
+class MainWindow(QMainWindow):
     u"""Entire gui stuff goes here."""
     def __init__(self):
-        super(MyWidget, self).__init__()
-        file = QtCore.QFile("mainwindow.ui")
-        file.open(QtCore.QFile.ReadOnly)
-        self.window = QUiLoader().load(file)
-        file.close()
+        super(MainWindow, self).__init__()
+        self.ui = Ui_MainWindow()
+        self.ui.setupUi(self)
+
+        self.ui.pushButton_1.clicked.connect(lambda name: self.get_directory_name(self.ui.lineEdit_1))  # Diamond Edition Exe
+        self.ui.pushButton_2.clicked.connect(lambda name: self.get_directory_name(self.ui.lineEdit_2))  # Local directory for diamond
+        self.ui.pushButton_3.clicked.connect(lambda name: self.get_directory_name(self.ui.lineEdit_3))  # EE exe
+        self.ui.pushButton_4.clicked.connect(lambda name: self.get_directory_name(self.ui.lineEdit_4))  # Local directory for EE
+        self.ui.pushButton_5.clicked.connect(lambda name: self.get_directory_name(self.ui.lineEdit_5))  # NWNTool dir
+
+    def get_directory_name(self, placeholder):
+        dialog = QFileDialog()
+        dialog.setFileMode(QFileDialog.FileMode.Directory)
+        path = dialog.getExistingDirectory(self, "Select Directory")
+        placeholder.setText(path)
+        logger.debug(f"Set Text {path} for {placeholder}")
 
 
 def install():
@@ -27,9 +39,8 @@ def main():
     u"""Main window for the entire program."""
     app = QApplication([])
     app.setStyle("Fusion")
-    widget = MyWidget()
-    widget.resize(800, 400)
-    widget.window.show()
+    widget = MainWindow()
+    widget.show()
     app.exec_()
 
 
